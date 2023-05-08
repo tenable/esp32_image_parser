d05e36849fada939df2e46bcae6819f594d9fcfb
samy kamkar <git@samy.pl>
2023-05-07 19:37:26 -0700

    add support for non-DMA DRAM data

--- esp32_image_parser.py
+++ esp32_image_parser.py
@@ -55,6 +55,7 @@ def image2elf(filename, output_file, verbose=False):
     # maps segment names to ELF sections
     section_map = {
         'DROM'                      : '.flash.rodata',
+        'BYTE_ACCESSIBLE, DRAM'     : '.dram0.data',
         'BYTE_ACCESSIBLE, DRAM, DMA': '.dram0.data',
         'IROM'                      : '.flash.text',
         #'RTC_IRAM'                  : '.rtc.text' TODO
